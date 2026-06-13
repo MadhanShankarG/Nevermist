808bd723278e7b5d59e9f5509647a6b74536e0be
Madhan Shankar G <madhanshankarg@gmail.com>
2026-06-13 23:07:50 +0530

    P 12 notion send change : removed reminder v3

--- src/lib/notion-send.ts
+++ src/lib/notion-send.ts
@@ -93,10 +93,7 @@ export async function sendSingleTask(
         ? {
             start: `${task.dueDate}T${task.dueTime}:00`,
             end: `${task.dueDate}T${getEndTime(task.dueTime, task.duration || 60)}:00`,
-            reminder: {
-              unit: 'minute' as const,
-              value: 30,
-            },
+            
           }
         : { start: task.dueDate }
       properties[dueDatePropName] = { date: dateValue }
